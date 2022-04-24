879515222df67b6d28074e3ab178e4fa150ed5d3
ruiwenge2 <85425620+ruiwenge2@users.noreply.github.com>
2022-04-24 23:13:27 +0000

    change something

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
   entry: './src/main.js',
   output: {
-    filename: 'main.js',
+    filename: 'js/game.js',
     path: path.resolve(__dirname, 'public'),
   },
 };
