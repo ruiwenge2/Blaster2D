a01d86ef05722cbbcab9351e790fff355618f487
ruiwenge2 <85425620+ruiwenge2@users.noreply.github.com>
2022-05-08 18:31:23 +0000

    tell ppl to open in new tab

---
+++ public/script.js
@@ -0,0 +1,11 @@
+function embedded() {
+  try {
+    return window.self !== window.top;
+  } catch(e) {
+    return true;
+  }
+}
+
+if(embedded()){
+  alertmodal("Hi!", "Please open this in a new tab for best results.", ok="Open").then(() => {window.open("https://blaster2d.ruiwenge2.repl.co")})
+}
