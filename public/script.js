60682d4c120302224e0015e952aca769ccaeb134
ruiwenge2 <85425620+ruiwenge2@users.noreply.github.com>
2022-08-03 02:30:12 +0000

    pwa!

--- public/script.js
+++ public/script.js
@@ -10,13 +10,21 @@ if(embedded() && location.hostname == "191bb644-256e-48ce-b188-a2dde4c165f4.id.r
   alertmodal("", "Please open this in a new tab for best results.", ok="Open").then(() => {window.open("https://blaster2d.ruiwenge2.repl.co" + location.pathname)});
 }
 
-if(homepage && loggedIn){
-  var div = document.getElementById("userActions");
-  var hidden = document.getElementById("hidden");
-  div.addEventListener("mouseover", function(){
-    hidden.style.display = "block";
-  });
-  div.addEventListener("mouseleave", function(){
-    hidden.style.display = "none";
+try {
+  if(homepage && loggedIn){
+    var div = document.getElementById("userActions");
+    var hidden = document.getElementById("hidden");
+    div.addEventListener("mouseover", function(){
+      hidden.style.display = "block";
+    });
+    div.addEventListener("mouseleave", function(){
+      hidden.style.display = "none";
+    });
+  }
+} catch(e){}
+
+if ("serviceWorker" in navigator) {
+  window.addEventListener('load', function() {
+    navigator.serviceWorker.register('/sw.js');
   });
 }
